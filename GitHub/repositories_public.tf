7502f46138f3ddd6d680371cf5a1c071237d74f2
NPastorale <npastorale@gmail.com>
2024-01-17 10:27:00 +0100

    clean up old repositories

--- GitHub/repositories_public.tf
+++ GitHub/repositories_public.tf
@@ -1,57 +1,19 @@
-module "ansible-playbooks" {
-  source          = "./modules/public_repo"
-  repository_name = "ansible-playbooks"
-}
-
-module "arch-installation" {
-  source          = "./modules/public_repo"
-  repository_name = "arch-installation"
-}
-
-module "archmirror-rsync-client" {
-  source          = "./modules/public_repo"
-  repository_name = "archmirror-rsync-client"
-}
-
-module "archmirror-rsync-server" {
-  source          = "./modules/public_repo"
-  repository_name = "archmirror-rsync-server"
-}
-
-module "bind9" {
-  source          = "./modules/public_repo"
-  repository_name = "bind9"
-}
-
-module "dependatest" {
-  source          = "./modules/public_repo"
-  repository_name = "dependatest"
+module "containers" {
+  source                 = "./modules/public_repo"
+  repository_name        = "containers"
+  repository_description = "Collection of home-grown, organic, freshly built containers"
 }
 
 module "dotfiles" {
   source                 = "./modules/public_repo"
   repository_name        = "dotfiles"
-  repository_description = "Personal dotfiles for Linux"
-}
-
-module "droidcam-launcher" {
-  source          = "./modules/public_repo"
-  repository_name = "droidcam-launcher"
-}
-
-module "dynbind" {
-  source          = "./modules/public_repo"
-  repository_name = "dynbind"
-}
-
-module "kubectl" {
-  source          = "./modules/public_repo"
-  repository_name = "kubectl"
+  repository_description = "My dotfiles collection"
 }
 
 module "kubernetes" {
-  source          = "./modules/public_repo"
-  repository_name = "kubernetes"
+  source                 = "./modules/public_repo"
+  repository_name        = "kubernetes"
+  repository_description = "YAML galore"
 }
 
 module "kubernetes-presentation" {
@@ -59,30 +21,22 @@ module "kubernetes-presentation" {
   repository_name = "kubernetes-presentation"
 }
 
-module "py-camp-rps" {
-  source          = "./modules/public_repo"
-  repository_name = "py-camp-rps"
-}
-
 module "razer-viper-ultimate" {
-  source          = "./modules/public_repo"
-  repository_name = "razer-viper-ultimate"
-}
-
-module "recetas" {
-  source          = "./modules/public_repo"
-  repository_name = "recetas"
-}
-
-module "rsync" {
   source                 = "./modules/public_repo"
-  repository_name        = "rsync"
-  repository_description = "An Alpine-based container able to execute rsync"
+  repository_name        = "razer-viper-ultimate"
+  repository_description = "Python script that sets dock colour depending on mouse battery level"
+}
+
+module "scripts" {
+  source                 = "./modules/public_repo"
+  repository_name        = "scripts"
+  repository_description = "Collection of useful scripts"
 }
 
 module "terraform" {
-  source          = "./modules/public_repo"
-  repository_name = "terraform"
+  source                 = "./modules/public_repo"
+  repository_name        = "terraform"
+  repository_description = "Terraform scripts for my infrastructure"
 }
 
 module "torrent-paradise" {
@@ -90,22 +44,14 @@ module "torrent-paradise" {
   repository_name = "torrent-paradise"
 }
 
-module "ubuntu-prep" {
-  source          = "./modules/public_repo"
-  repository_name = "ubuntu-prep"
-}
-
 module "web" {
-  source          = "./modules/public_repo"
-  repository_name = "web"
+  source                 = "./modules/public_repo"
+  repository_name        = "web"
+  repository_description = "Personal website accessible at nahue.ar"
 }
 
 module "workflows" {
-  source          = "./modules/public_repo"
-  repository_name = "workflows"
-}
-
-module "scripts" {
-  source          = "./modules/public_repo"
-  repository_name = "scripts"
+  source                 = "./modules/public_repo"
+  repository_name        = "workflows"
+  repository_description = "Reusable GitHub Actions Workflows"
 }
