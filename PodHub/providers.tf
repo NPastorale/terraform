02ba510aa412d32cf8482caa4c9657b9a5841751
NPastorale <npastorale@gmail.com>
2024-01-17 10:24:54 +0100

    bump version to 0.4.0

--- PodHub/providers.tf
+++ PodHub/providers.tf
@@ -9,7 +9,7 @@ terraform {
   required_providers {
     talos = {
       source  = "siderolabs/talos"
-      version = "0.2.0"
+      version = "0.4.0"
     }
     helm = {
       source  = "hashicorp/helm"
