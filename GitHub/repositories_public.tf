6f3a15e88ab98f11f64a9dd1dfc4781a7135a7cd
NPastorale <npastorale@gmail.com>
2024-02-05 10:08:19 +0100

    add test repositories

--- GitHub/repositories_public.tf
+++ GitHub/repositories_public.tf
@@ -55,3 +55,21 @@ module "workflows" {
   repository_name        = "workflows"
   repository_description = "Reusable GitHub Actions Workflows"
 }
+
+module "renovate-test-terraform" {
+  source          = "./modules/repository"
+  repository_name = "renovate-test-terraform"
+  main_force_push = true
+}
+
+module "renovate-test-kubernetes" {
+  source          = "./modules/repository"
+  repository_name = "renovate-test-kubernetes"
+  main_force_push = true
+}
+
+module "renovate-test-docker" {
+  source          = "./modules/repository"
+  repository_name = "renovate-test-docker"
+  main_force_push = true
+}
